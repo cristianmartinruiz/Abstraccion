9c0cb6a2539b3024a2f120b44a11828d92b8b35d
cristianmartinruiz <cristian20martin20@gmail.com>
2024-11-14 20:33:29 +0100

    Create vehiculo.py

---
+++ vehiculo.py
@@ -0,0 +1,46 @@
+class vehiculo:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+        self.encendido = False
+        self.velocidad = 0
+
+    def mostrar_vehiculo(self):
+        print(f"Este es un {self.marca}; {self.modelo}")
+
+    def encender (self):
+        self.encendido = True
+        print (f"El vehiculo esta encendido")
+
+    def apagado (self):
+        self.encendido = False
+        print (f"El vehiculo esta apagado")
+
+    def acelerar(self):
+        if self.encendido :
+            self.velocidad += incremento
+            print (f"Vehiculo acelerado {self.velocidad} km/h")
+        
+        else:
+            print (f"No se puede acelerar, encienda el vehiculo") 
+
+    def frenar (self,decremento):
+        if self.encendido:
+            if self.velocidad - decremento >= 0:
+                self.velocidad = decremento
+                print (f"El vehiculo desacelero a: {self.velocidad} km/h")
+
+            else:
+                self.velocidad = 0
+                print (f"El vehiculo se detuvo")
+
+        else:
+            print (f"No se puede frenar, encienda el vehiculo primero.")
+
+vehiculo1 = vehiculo("Toyota","Corolla")
+
+
+vehiculo1.mostrar_vehiculo()
+vehiculo1.encender()
+vehiculo1.frenar(10)
+vehiculo1.apagado()
